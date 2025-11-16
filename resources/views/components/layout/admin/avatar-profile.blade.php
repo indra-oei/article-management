adb3d543bf6ccca72319996b1f1eb1b8ef54c9a4
Indra Oei <indraoei25@gmail.com>
2025-11-17 01:50:13 +0700

    ui: change avatar link

--- resources/views/components/layout/admin/avatar-profile.blade.php
+++ resources/views/components/layout/admin/avatar-profile.blade.php
@@ -7,7 +7,7 @@
         x-transition
         class="absolute right-0 mt-1 w-48 bg-white rounded-md shadow-lg z-50"
     >
-        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Profile</a>
-        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</a>
+        <a href="#" class="block px-4 py-2 text-sm text-gray-600 hover:bg-gray-100">Change Password</a>
+        <a href="#" class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100">Log Out</a>
     </div>
 </div>
